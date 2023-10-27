a1db8983f24790fc37855f9a65b10f1b0ac06265
untodesu <41549137+untodesu@users.noreply.github.com>
2023-10-27 12:17:03 +0500

    process_ycbcr.sh: name channel files accordingly

--- DIMPROC/homework/process_ycbcr.sh
+++ DIMPROC/homework/process_ycbcr.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # Split panchromatic (grayscale) image into YCbCr channels
-magick src_pan.bmp -colorspace YCbCr -channel 0 -separate int_pan_L.tif
-magick src_pan.bmp -colorspace YCbCr -channel 1 -separate int_pan_A.tif
-magick src_pan.bmp -colorspace YCbCr -channel 2 -separate int_pan_B.tif
+magick src_pan.bmp -colorspace YCbCr -channel 0 -separate int_pan_Y.tif
+magick src_pan.bmp -colorspace YCbCr -channel 1 -separate int_pan_Cb.tif
+magick src_pan.bmp -colorspace YCbCr -channel 2 -separate int_pan_Cr.tif
 
 # Split multichannel (colored) image into YCbCr channels
 # NOTE: the multichannel is assumed to be twice as small as panchromatic
-magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 0 -separate int_multi_L.tif
-magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 1 -separate int_multi_A.tif
-magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 2 -separate int_multi_B.tif
+magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 0 -separate int_multi_Y.tif
+magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 1 -separate int_multi_Cb.tif
+magick src_multi.bmp -resize 200% -colorspace YCbCr -channel 2 -separate int_multi_Cr.tif
 
 # Combine panchromatic luminance and multichannel chrominance
-magick int_pan_L.tif int_multi_A.tif int_multi_B.tif -set colorspace YCbCr -combine int_result.tif
+magick int_pan_Y.tif int_multi_Cb.tif int_multi_Cr.tif -set colorspace YCbCr -combine int_result_ycbcr.tif
 
 # Convert to a more user-friendly format and colorspace
-magick int_result.tif -colorspace sRGB result.png
+magick int_result_ycbcr.tif -colorspace sRGB result_ycbcr.png
 
